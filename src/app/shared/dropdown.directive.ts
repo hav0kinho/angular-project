c387aa6c36d20b6261b447f654eb5add68577c0d
hav0kinho <ruallysonfelype@gmail.com>
2024-01-24 21:44:45 -0300

    106 - Adicionando alguns comentários

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -4,11 +4,11 @@ import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core"
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  @HostBinding('class.open') isOpen = false;
+  @HostBinding('class.open') isOpen = false; // Se for true, adiciona uma classe chamada "open" na <tag> ou componente!
 
-  constructor(private elRef: ElementRef){}
+  constructor(private elRef: ElementRef){} // Injeta a referêncido elemento atrelado.
 
-  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) { // Configura um evento no document, que será ativado caso tenha algum clique! Passa o evento para a função.
     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
 }
