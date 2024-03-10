cdf2fdee161e8e1cbdfa3124cd8f95f9913f25b3
hav0kinho <ruallysonfelype@gmail.com>
2024-03-10 18:43:54 -0300

    123 - Adding the Shopping List Service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
-})
-export class ShoppingListComponent {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+  styleUrl: './shopping-list.component.css',
 
-  onIngredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient);
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[];
+
+  constructor(private slService: ShoppingListService){
+
+  }
+
+  ngOnInit(){
+    this.ingredients = this.slService.getIngredients();
   }
 }
