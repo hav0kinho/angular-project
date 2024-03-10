825a26c8cb02c1b8d247407733e4b8d2a196ac47
hav0kinho <ruallysonfelype@gmail.com>
2024-03-10 18:53:44 -0300

    125. Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,12 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipesService{
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simply a test', 'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg'),
+    new Recipe('A Test Recipe', 'This is a simply a test', 'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
   ];
 
 
